e4bed2c63bc1f46a818759fb8761e4b3e6735791
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-08-21 19:12:34 -0400

    finished app backend

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const products = express.Router();
 
-const { getAllProducts, getOneProduct } = require("../queries/products.js");
+const {
+  getAllProducts,
+  getOneProduct,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} = require("../queries/products.js");
 
 //INDEX
 products.get("/", async (req, res) => {
@@ -14,14 +20,47 @@ products.get("/", async (req, res) => {
 });
 
 //SHOW
-products.get("/:id", async(req, res) => {
-    const { id } = req.params;
-    const oneProduct = await getOneProduct(id);
-    if(oneProduct) {
-        res.status(200).json(oneProduct)
-    } else {
-        res.status(404).json({error: "This ID is not exist"})
-    }
-})
+products.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const oneProduct = await getOneProduct(id);
+  if (oneProduct) {
+    res.status(200).json(oneProduct);
+  } else {
+    res.status(404).json({ error: "This ID is not exist" });
+  }
+});
+
+// CREATE (POST)
+products.post("/", async (req, res) => {
+  try {
+    const newProduct = await createProduct(req.body);
+    res.json(newProduct);
+  } catch (error) {
+    res.status(404).json({ error: "server error" });
+  }
+});
+
+// DELETE
+products.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await deleteProduct(id);
+  if (deletedProduct) {
+    res.status(200).json(deletedProduct);
+  } else {
+    res.status(404).json({ error: "server error" });
+  }
+});
+
+// UPDATED (PUT)
+
+products.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const updatedProduct = await updateProduct(id, req.body);
+//   if (updatedProduct) {
+    res.status(200).json(updatedProduct);
+//   } else {
+//     res.status(404).json({ error: "server error" });
+//   }
+});
 
 module.exports = products;
--- queries/products.js
+++ queries/products.js
@@ -11,7 +11,7 @@ const getAllProducts = async () => {
   }
 };
 
-// GET ALL PRODUCTS
+// GET ONE PRODUCTS
 const getOneProduct = async (id) => {
   try {
     const oneProducts = await db.one(
@@ -20,15 +20,71 @@ const getOneProduct = async (id) => {
     );
     return oneProducts;
   } catch (error) {
-    console.error("Error fetching all cities:", error);
     return error;
   }
 };
 
+//CREATE (POST)
+const createProduct = async (product) => {
+  try {
+    const newProduct = await db.one(
+      "INSERT INTO cookie_candy (product_name, product_type, calories, price, last_updated, url, is_favorite) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [
+        product.product_name,
+        product.product_type,
+        product.calories,
+        product.price,
+        product.last_updated,
+        product.url,
+        product.is_favorit,
+      ]
+    );
+    return newProduct
+  } catch (error) {
+    return error;
+  }
+};
 
+// DELETE
+const deleteProduct = async (id) => {
+  try {
+    const deletedProduct = await db.one(
+      "DELETE FROM cookie_candy WHERE id = $1 RETURNING *",
+      id
+    );
+    return deletedProduct
+  } catch (error) {
+    return error;
+  }
+};
 
+// UPDATE (PUT)
+
+const updateProduct = async (id, product) => {
+    try {
+      const updatedProduct = await db.one(
+        "UPDATE cookie_candy SET product_name=$1, product_type=$2, calories=$3, price=$4, last_updated=$5, url=$6, is_favorite=$7 WHERE id=$8  RETURNING *",
+        [
+          product.product_name,
+          product.product_type,
+          product.calories,
+          product.price,
+          product.last_updated,
+          product.url,
+          product.is_favorit,
+          id,
+        ]
+      );
+      return updatedProduct;
+    } catch (error) {
+      return error;
+    }
+  };
 
 module.exports = {
   getAllProducts,
   getOneProduct,
+  createProduct,
+  deleteProduct,
+  updateProduct,
 };
